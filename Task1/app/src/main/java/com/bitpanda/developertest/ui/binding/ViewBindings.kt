c840eb00fbad1f409bc01be0b0e6688e521b7164
mzielu <kmarcinzielinski@gmail.com>
2021-01-25 00:20:31 +0100

    Add library for loading svg files

--- Task1/app/src/main/java/com/bitpanda/developertest/ui/binding/ViewBindings.kt
+++ Task1/app/src/main/java/com/bitpanda/developertest/ui/binding/ViewBindings.kt
@@ -1,12 +1,26 @@
 package com.bitpanda.developertest.ui.binding
 
+import android.R
+import android.graphics.drawable.PictureDrawable
+import android.net.Uri
+import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bitpanda.developertest.model.FilterType
 import com.bitpanda.developertest.ui.widgets.FilterTypesView
+import com.github.twocoffeesoneteam.glidetovectoryou.GlideToVectorYou
+
 
 @BindingAdapter("filterChangeAction")
 fun setLineClickAction(view: FilterTypesView, action: (FilterType) -> Unit) {
     view.onFilterChangeAction {
         action(it)
     }
+}
+
+@BindingAdapter("imageUrl")
+fun loadImage(view: ImageView, url: String) {
+    GlideToVectorYou
+        .init()
+        .with(view.context)
+        .load(Uri.parse(url), view)
 }
